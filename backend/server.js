18ef6aed8e17326634773a4547fbbdb0cd9328e1
Mohak-Singhal <mohaksinghal516@gmail.com>
2024-11-13 12:38:39 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,8 @@ const port = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-    origin: 'https://food-delivery-oba0.onrender.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-  }));
 
+app.use(cors());
 //db connection
 connectDB();
 //api endpoints
